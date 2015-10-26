984786045cba7156c848379866a19c719dab71a0
Sam CHAN <swh5a01@hotmail.com>
2015-10-26 11:15:43 +0800

    1st Version of WebView testing
    (cherry picked from commit 75f97022b82454ecf30ae721ce675c8ac55fc981)
    
    # Conflicts:
    #	app/src/main/java/com/example/sam/webviewtest/MainActivity.java

--- app/src/main/java/com/example/sam/webviewtest/MainActivity.java
+++ app/src/main/java/com/example/sam/webviewtest/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setUserAgentString("Mozilla/5.0 (Linux; U; Android 4.4.4; fr-fr; Nexus 5 Build/KTU84P) AppleWebKit/537.16 (KHTML, like Gecko) Version/4.0 Safari/537.16"); //user agent testing
 
         myWebView.setWebViewClient(new WebViewClient());                        //set the webview load inside the app
-        myWebView.loadUrl("http://www.linepost.hk");      //load the url of the webview
+        myWebView.loadUrl("http://www.google.com");      //load the url of the webview
     }
 
 
