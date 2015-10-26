19f2086c41167fb6d4f3f0fad639a14e13f07c8f
Sam CHAN <swh5a01@hotmail.com>
2015-10-26 09:36:54 +0800

    Update comments

--- app/src/main/java/com/example/sam/webviewtest/MainActivity.java
+++ app/src/main/java/com/example/sam/webviewtest/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         WebView myWebView = (WebView) findViewById(R.id.webview);               //ping the webview container id
         WebSettings webSettings = myWebView.getSettings();                      //get webview settings initiated
         webSettings.setJavaScriptEnabled(true);                                 //enable javascript
-        webSettings.setUserAgentString("Mozilla/5.0 (Linux; U; Android 4.4.4; fr-fr; Nexus 5 Build/KTU84P) AppleWebKit/537.16 (KHTML, like Gecko) Version/4.0 Safari/537.16");
+        webSettings.setUserAgentString("Mozilla/5.0 (Linux; U; Android 4.4.4; fr-fr; Nexus 5 Build/KTU84P) AppleWebKit/537.16 (KHTML, like Gecko) Version/4.0 Safari/537.16"); //user agent testing
         myWebView.setWebViewClient(new WebViewClient());                        //set the webview load inside the app
         myWebView.loadUrl("http://www.linepost.hk");      //load the url of the webview
     }
